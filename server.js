d85d41a04dfa87b7be329b5fe2b36e92b57e8c9f
RickyCohen <rickycohen88@gmail.com>
2021-02-05 16:29:10 -0600

    server is now functioning

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
+//dependency import
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
 
-
+//create//assign express server
 const server = express();
+// set port to listen on
+const PORT = process.env.PORT || 2727;
+//middleware for the server to read and write in the format needed where and when.
+server.use(express.urlencoded({extended:true}));
+server.use(express.json());
+
+// tell server to use morgan with tiny preset
 server.use(morgan('tiny'));
+
+server.listen(PORT,()=>{console.log("server is listening on port" + PORT)})
