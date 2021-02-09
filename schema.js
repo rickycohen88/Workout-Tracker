bbecb6e17a855f1a043f43297fe4e7808c71bd3a
Richard Cohen <richard.cohen@ericsson.com>
2021-02-09 16:37:38 -0600

    brining hub up to date with laptop so i can move to desktop

--- schema.js
+++ schema.js
@@ -1,10 +1,25 @@
 //dependencies
 const mongoose = require('mongoose');
-const {cShema} = mongoose;
+const {Schema} = mongoose;
 
-const workout = new Schema({
-    day:{Type:Date,
-         Default: Date.now
+const workoutSchema = new Schema({
+    day:{
+            Type:Date,
+            Default: Date.now
         },
-    
+    exercises:[
+        {
+        type :{type: String, required: true},
+        name :{type: String, required : true},
+        duration :{type: Number},
+        weight : {type: Number},
+        reps : {type: Number},
+        sets : {type: Number},
+        distance : {type: Number}
+        }
+    ]
 })
+
+const workoutExercise = mongoose.model('workout',workoutSchema);
+
+module.exports = workoutExercise;
